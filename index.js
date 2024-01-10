cee6657e133243011696a2cf25d0f7697dabac32
Rururuisi <louiseliang9943@gmail.com>
2024-01-10 18:07:33 -0500

    theme selection toggle active

--- index.js
+++ index.js
@@ -6,17 +6,21 @@
 		return document.querySelectorAll(target);
 	};
 
+	const changeActiveObj = (items, idx) => {
+		items.forEach((item, i) => {
+			if (idx === i) {
+				item.classList.add("active");
+			} else {
+				item.classList.remove("active");
+			}
+		});
+	};
+
 	// --------------------Menu Items Activation Handling--------------------
 	const menuItems = getAll(".menu-item");
 	menuItems.forEach((menuItem, idx) => {
 		menuItem.addEventListener("click", () => {
-			menuItems.forEach((item, i) => {
-				if (idx === i) {
-					item.classList.add("active");
-				} else {
-					item.classList.remove("active");
-				}
-			});
+			changeActiveObj(menuItems, idx);
 
 			// --------------------Toggle Notification Pannel--------------------
 			if (menuItem.id !== "notification-menu-item") {
@@ -27,13 +31,26 @@
 		});
 	});
 
-	// --------------------Toggle Theme Customized Pannel--------------------
-	get("#theme-menu-item").addEventListener("click", () => {
-		get(".customize-theme").style.display = "grid";
+	// ---------------------------Messages---------------------------
+	const msgNotif = get("#messages-notifications");
+	msgNotif.addEventListener("click", () => {
+		get(".messages").style.boxShadow = "0 0 1rem var(--color-primary)";
+		msgNotif.querySelector(".notification-count").style.display = "none";
+		setTimeout(() => {
+			get(".messages").style.boxShadow = "none";
+		}, 2000);
 	});
 
-	get(".close-theme").addEventListener("click", () => {
-		get(".customize-theme").style.display = "none";
+	// search for messages
+	get("#messages").addEventListener("keyup", (evt) => {
+		getAll(".message").forEach((msg) => {
+			const user = msg.querySelector("h5").innerText.toLowerCase();
+			if (user.includes(evt.target.value.toLowerCase())) {
+				msg.style.display = "flex";
+			} else {
+				msg.style.display = "none";
+			}
+		});
 	});
 
 	// --------------------Toggle Sidebar on Mobile Screen--------------------
@@ -47,5 +64,37 @@
 			menuToggler.classList.remove("active");
 			get(".notifications-popup").classList.remove("active");
 		}
+	});
+
+	// --------------------Toggle Theme Customized Pannel--------------------
+	get("#theme-menu-item").addEventListener("click", () => {
+		get(".customize-theme").style.display = "grid";
+	});
+
+	get(".close-theme").addEventListener("click", () => {
+		get(".customize-theme").style.display = "none";
+	});
+
+	// --------------------Handle Theme Customization--------------------
+	const fontSizes = get(".choose-size").querySelectorAll("span");
+	const colors = get(".choose-color").querySelectorAll("span");
+	const bgs = get(".choose-bg").querySelectorAll("div");
+
+	fontSizes.forEach((size, idx) => {
+		size.addEventListener("click", () => {
+			changeActiveObj(fontSizes, idx);
+		});
+	});
+
+	colors.forEach((color, idx) => {
+		color.addEventListener("click", () => {
+			changeActiveObj(colors, idx);
+		});
+	});
+
+	bgs.forEach((bg, idx) => {
+		bg.addEventListener("click", () => {
+			changeActiveObj(bgs, idx);
+		});
 	});
 })();
